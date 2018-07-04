5cfc9752da2d2632ea84f0679e2c1b6d59c9d3db
Sean Rankine <sean.rankine@me.com>
2018-07-04 01:37:54 -0400

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 import { Container, Aside, Main, Tag } from 'element-ui'
+import VueAnalytics from 'vue-analytics'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Container)
 Vue.use(Tag)
 Vue.use(Aside)
 Vue.use(Main)
+
+Vue.use(VueAnalytics, {
+  id: process.env.VUE_APP_TRACKING_ID
+})
 
 Vue.config.productionTip = false
 
