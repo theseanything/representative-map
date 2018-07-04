e49391cd658044f33d1e34cee37aa258c7ca8c23
Sean Rankine <sean.rankine@me.com>
2018-07-03 23:34:49 -0400

    Working version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,153 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <el-container>
+      <el-aside>
+        <h2>NYS Senate Map</h2>
+          <GmapAutocomplete @place_changed="setPlace">
+          </GmapAutocomplete>
+        <div v-if="selectedDistrict">
+          <h3>District {{ selectedDistrict.number }}</h3>
+          <img :src="selectedDistrict.senator.headshot" alt="" width="160" height="160">
+          <h3>{{ selectedDistrict.senator.name }}</h3>
+          <div id="parties">
+            <el-tag type="info" size="small" v-for="p in selectedDistrict.senator.parties" :key="p">{{ getPartyFullname(p) }}</el-tag>
+          </div>
+        </div>
+        <div v-if="!selectedDistrict">
+          <h3>No District Selected</h3>
+        </div>
+
+      </el-aside>
+      <el-main id="main">
+        <gmap-map id="map" ref="mapRef" :center="center" :zoom="7" :options="{mapTypeControl: false, streetViewControl: false}">
+          <GmapMarker 
+            v-if="this.place" 
+            label="★" 
+            :position="{lat: this.place.geometry.location.lat(),lng: this.place.geometry.location.lng()}"
+          />
+
+          <gmap-polygon v-for="d in senateDistricts" :ref="'polygon' + d.number" :key="d.number" :paths="d.coordinates" :options="d.options" @click="selectDistrict(d)">
+          </gmap-polygon>
+        </gmap-map>
+      </el-main>
+    </el-container>
   </div>
+
 </template>
 
 <script>
+import sentateInfo from '../assets/senateDistricts.json'
+import senators from '../assets/senators.json'
+import partyInfo from '../assets/partyInformation.json'
+import { gmapApi } from 'vue2-google-maps'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  computed: {
+    google: gmapApi
+  },
+  data () {
+    return {
+      center: { lat: 42.7466321, lng: -75.770041 },
+      place: null,
+      senateDistricts: [],
+      selectedDistrict: null,
+      partyInfo: partyInfo
+    }
+
+  },
+  methods: {
+    setPlace (place) {
+      this.place = place
+      var point = new this.google.maps.LatLng(this.place.geometry.location.lat(), this.place.geometry.location.lng())
+      for (var key in this.$refs) {
+        if (key.startsWith('polygon')) {
+          var polygon = this.$refs[key][0].$polygonObject
+          if (this.google.maps.geometry.poly.containsLocation(point, polygon)) {
+            var districtNumber = key.substring(7)
+            this.selectDistrict(this.senateDistricts.find(d => { return d.number == districtNumber }))
+            return
+          }
+        }
+      }
+
+      // var point = new this.google.maps.LatLng(this.place.geometry.location.lat(), this.place.geometry.location.lng())
+      // this.google.maps.geometry.poly.containsLocation(point, polygon)
+    },
+    getPartyFullname (p) {
+      var party = this.partyInfo[p]
+      return (party) ? party.name : p
+    },
+    highlightDistrict (d) {
+      d.options.strokeWeight = 3
+      d.options.fillOpacity = 0.3
+    },
+    unhighlightDistrict (d) {
+      d.options.strokeWeight = 1
+      d.options.fillOpacity = 0.1
+    },
+    selectDistrict (d) {
+      if (this.selectedDistrict) {
+        this.unhighlightDistrict(this.selectedDistrict)
+      }
+      this.selectedDistrict = (this.selectedDistrict == d) ? null : d
+      if (this.selectedDistrict) {
+        this.highlightDistrict(this.selectedDistrict)
+      }
+    },
+    getBoundaryColor (parties) {
+      if (parties.includes('D')) {
+        return this.partyInfo['D'].color
+      } else if (parties.includes('R')) {
+        return this.partyInfo['R'].color
+      }
+      return '#000000'
+    }
+
+  },
+  created () {
+    sentateInfo.districts.forEach(d => {
+
+      var senator = senators.senators.find(s => {
+        return s.district === d.number
+      })
+
+      var color = this.getBoundaryColor(senator.parties)
+
+      this.senateDistricts.push({
+        number: d.number,
+        coordinates: d.coordinates,
+        options: { geodesic: true, strokeColor: color, fillColor: color, strokeWeight: 1, fillOpacity: 0.1, strokeOpacity: 0.2 },
+        senator: senator
+      })
+    })
+  },
+  // mounted () {
+  //   this.$refs.mapRef.$mapPromise.then((map) => {
+  //     var bounds = new this.google.maps.LatLngBounds({ lat: 40.4773991, lng: -79.7625901 }, { lat: 45.015865, lng: -71.777491 })
+  //     map.fitBounds(bounds)
+  //     map.panToBounds(bounds)
+  //   })
+  // }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+#main {
   padding: 0;
+  height: 100vh;
+  width: 100%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#map {
+  height: 100%;
+  width: 100%;
 }
-a {
-  color: #42b983;
+
+.hello {
+  height: 100%;
+  width: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import { Container, Aside, Main, Tag } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Container)
+Vue.use(Tag)
+Vue.use(Aside)
+Vue.use(Main)
 
 Vue.config.productionTip = false
+
+// Vue.use(MdOption)
+// Vue.use(MdSelect)
+console.log(process.env.VUE_APP_GMAP_KEY)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GMAP_KEY,
+    // key: 'AIzaSyANsnW141BsRr4JFQ5EACU5PR3GNxGGqps',
+    libraries: 'places,geometry',
+    v: '3.32' // necessary for places input
+  }
+})
 
 new Vue({
   render: h => h(App)
